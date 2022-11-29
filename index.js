7e2580732608fc6f71519dd7bf38db11bc6def89
Soroush Habibi <soroush.habibi.1382@gmail.com>
2022-11-29 21:59:58 +0330

    write createUser method for db.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import 'dotenv/config';
 
+import DB from './models/db.js';
+
 const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+DB.connect((client) => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+    DB.createUser("Soroush", "sshh1382").catch((e) => {
+        console.log(e);
+    });
+}).catch(e => {
+    console.log(e.message);
 });
---
+++ models/db.js
@@ -0,0 +1,67 @@
+import mongodb from 'mongodb';
+import bcrypt from 'bcrypt';
+
+export default class DB {
+    static async connect(func) {
+        this.client = await mongodb.MongoClient.connect(process.env.MONGODB_URL);
+        func(this.client);
+    }
+
+    static async existsUsername(username) {
+        const result = await this.client.db("Food").collection("users").countDocuments({ username: username });
+        return result ? true : false;
+    }
+
+    static async existsEmail(email) {
+        const result = await this.client.db("Food").collection("users").countDocuments({ email: email });
+        return result ? true : false;
+    }
+
+    static async createUser(fullname, password) {
+        let studentId, foodCode;
+        if (fullname.length < 7) {
+            throw "fullname should be greater than 6 characters";
+        } if (password.length < 8) {
+            throw "password should be at least 7 characters";
+        }
+        if (await this.client.db("Food").collection("users").countDocuments({}) === 0) {
+            studentId = 40013131000;
+            foodCode = 122000;
+        } else {
+            await this.client.db("Food").collection("users").aggregate([
+                {
+                    $sort: {
+                        student_id: -1
+                    }
+                }, {
+                    $limit: 1
+                }
+            ]).forEach(value => studentId = value.student_id + 1);
+
+            await this.client.db("Food").collection("users").aggregate([
+                {
+                    $sort: {
+                        food_code: -1
+                    }
+                }, {
+                    $limit: 1
+                }
+            ]).forEach(value => foodCode = value.food_code + 1);
+        }
+
+        const result = await this.client.db("Food").collection("users").insertOne({
+            student_id: studentId,
+            food_code: foodCode,
+            food_code_password: 1,
+            fullname: fullname,
+            username: studentId,
+            password: bcrypt.hashSync(password, 10),
+            email: null,
+            currency: 0,
+            currency_transactions: [],
+            food_reserves: []
+        });
+
+        return result;
+    }
+}
